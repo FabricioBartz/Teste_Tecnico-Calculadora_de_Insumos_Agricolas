c6b9581460a4fde07304603accf79dfc78691b5a
Fabricio Fiss Bartz <fabriciofissbartz@gmail.com>
2026-01-29 20:44:48 -0300

    ajustes no hover e trava de numeros maximos

--- calculadora_de_insumos_agriculas/src/components/InputArea/InputArea.jsx
+++ calculadora_de_insumos_agriculas/src/components/InputArea/InputArea.jsx
@@ -1,12 +1,14 @@
 //função para inserir a área em hectares
 import styles from "./InputArea.module.css";
 
-export default function InputArea({ label, value, onChange, erro }) { 
+export default function InputArea({ label, value, onChange, erro }) {
   return (
     <div className={styles.container}>
       <label className={`${styles.label} body-medium semibold`}>{label}</label>
       {/* Wrapper necessário para posicionar o ícone de aviso sobre o input */}
-      <div className={`${styles.inputWrapper} ${erro ? styles.wrapperErro : ""}`}>
+      <div
+        className={`${styles.inputWrapper} ${erro ? styles.wrapperErro : ""}`}
+      >
         <input
           // Se houver erro, uma classe extra para a borda ficar vermelha
           className={`${styles.input} ${erro ? styles.inputErro : ""} body-medium`}
@@ -15,12 +17,15 @@ export default function InputArea({ label, value, onChange, erro }) {
           min="0"
           value={value}
           onChange={(e) => {
-              const val = e.target.value;
-              // trava de não negativos
-              if (val >= 0 || val === "") onChange(val);
-            }}
-          />
-        </div>
+            const val = e.target.value;
+            // 1. Permite se o valor for maior/igual a zero ou vazio (trava de negativos)
+            // 2. E (&&) apenas se o texto tiver 7 caracteres ou menos (trava de comprimento)
+            if ((val >= 0 || val === "") && val.length <= 7) {
+              onChange(val);
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
